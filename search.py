440d865e80568b08929da01211fc4b4e3d677a95
marble <msm.mirdamad@gmail.com>
2026-05-02 23:03:59 +0330

    ucs is working!

--- search.py
+++ search.py
@@ -1,4 +1,7 @@
+import heapq
 from collections import deque
+from enum import unique
+
 
 def bfs_solve(game):
     start_state = game.get_initial_state()
@@ -40,9 +43,35 @@ def ids_solve(game):
 
 def ucs_solve(game):
     start_state = game.get_initial_state()
+    node_solution = {start_state: []}
 
-    # Your code goes here
-    # this function should return actions
+    if game.is_goal(start_state):
+        return node_solution[start_state]
+
+    frontier = []
+    unique_str = str(start_state.player) + str(start_state.boxes)
+    heapq.heappush(frontier, (0, 0, unique_str, start_state))
+    explored = set()
+    explored.add(start_state)
+
+    while frontier:
+        current_cost, current_step, current_unique_str, current_state = heapq.heappop(frontier)
+
+        for action, next_state, next_cost in game.get_successors(current_state):
+            solution = node_solution[current_state]
+            next_cost += current_cost
+            next_step = current_step + 1
+
+            if next_state not in explored:
+                explored.add(next_state)
+                node_solution[next_state] = solution + [action]
+
+                if game.is_goal(next_state):
+                    return node_solution[next_state]
+
+                unique_str = str(next_state.player) + str(next_state.boxes)
+                heapq.heappush(frontier, (next_cost, next_step, unique_str, next_state))
+    return None
 
 
 def astar_solve(game):
